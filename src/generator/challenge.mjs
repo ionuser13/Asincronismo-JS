d564f547176d38c9b0e5c60875f45f66ed31720c
userjohn <johnchacpi479@gmail.com>
2022-10-18 14:06:02 -0500

    commented the generator challenge

--- src/generator/challenge.mjs
+++ src/generator/challenge.mjs
@@ -1,23 +1,23 @@
 import fetch from "node-fetch"
 
-const API = "https://api.escuelajs.co/api/v1";
+const API = "https://api.escuelajs.co/api/v1";//stores the api url
 
-async function fetchData(urlApi) {//fetchData is declared as a generator function
-    const response = await fetch(urlApi);
-    const data = await response.json();
-    return data;
+async function fetchData(urlApi) {//fetchData is declared as an async function
+    const response = await fetch(urlApi);//waits for extracting data from the api and is stored it in a variable, "response"
+    const data = await response.json();//that data is converted to a json object and is stored in data
+    return data;//the function returns that json object
 }
 
 async function* iterate(urlApi) {
     try {
-        const products = await fetchData(`${urlApi}/products`);
-        yield console.log(products[57])
+        const products = await fetchData(`${urlApi}/products`);//extracts the list of products from the api and stores it in "products"
+        yield console.log(products[57])//pauses and returns the item n57 in products
 
-        const product = await fetchData(`${urlApi}/products/${products[57].id}`);
-        yield console.log(product.description)
+        const product = await fetchData(`${urlApi}/products/${products[57].id}`);//extracts the product with the given id from the api
+        yield console.log(product.description)//pauses and returns the description of the item, in this case the product 57
 
-        const category = await fetchData(`${urlApi}/products/${product.category.id}`);
-        yield console.log(category.name)
+        const category = await fetchData(`${urlApi}/products/${product.category.id}`);////extracts the id of the category of the product we called
+        yield console.log(category.name)// pauses and returns the name of the category of the product
 
     }
     catch (error){
